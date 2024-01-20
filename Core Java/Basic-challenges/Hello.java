af4f77b0a9fba3c48e49a51c92f7f6dd2c497357
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-01-20 19:44:34 +0530

    basic concepts

---
+++ Core Java/Basic-challenges/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args){
+       int number1=1;
+       int number2=2;
+       int result= number1+number2;
+        System.out.println(result);
+    }
+    
+} 
