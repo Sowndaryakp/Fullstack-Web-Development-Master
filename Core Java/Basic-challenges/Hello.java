f5befc32758419888499a9993cd45b7f5919ccd8
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-01-22 21:48:56 +0530

    basic codes

--- Core Java/Basic-challenges/Hello.java
+++ Core Java/Basic-challenges/Hello.java
@@ -1,9 +1,52 @@
 public class Hello{
     public static void main(String[] args){
-       int number1=1;
-       int number2=2;
-       int result= number1+number2;
+        //variables
+        int number1=1;
+        int number2=2;
+        int result= number1+number2;
         System.out.println(result);
+
+        //data types
+        // int num1=10;
+        // byte by=127;
+        // short sh=558;
+        // long l=5854l;
+
+        // float f=5.8f;
+        // double d=5.8;
+
+        // char c='k';
+
+        // boolean b=true;
+
+        //literals
+        // int num=0b101;
+        // int num=0x7E;
+        // System.out.println(num);
+
+        //Type conversion and casting
+        // byte b =127;
+        // int a = b;
+        // System.out.println(a);
+
+        // int a = 127;
+        // byte k= a; // getting error bcz cannot convert from int to byte 
+        // System.out.println(k);
+
+        // int a =257;
+        // byte k = (byte) a;
+        // System.out.println(k);
+
+        // float f=5.6f;
+        // int t=(int)f;
+        // System.out.println(t);
+
+        //type promotion
+        byte a=10;
+        byte b=30;
+        int res= a*b;
+        System.out.println(res);
+
     }
     
 } 
