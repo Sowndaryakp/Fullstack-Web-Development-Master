c0bbb1bec071573b7b6295b8356ee531a2004442
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-02-01 07:01:17 +0530

    map method

---
+++ JavaScriptFullstack/Learning-kit/Practice/forEachMethod.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3];
+arr.forEach((e)=>
+{
+    console.log(e* 2);
+}
+)
+console.log(arr);
---
+++ JavaScriptFullstack/Learning-kit/Practice/mapMethod.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3];
+let mapArray = arr.map((e) =>
+    e*2);
+console.log(mapArray);
+console.log(arr);
