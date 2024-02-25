1d33fbaa6ea3e4d3d937ef812f0758f9911f5219
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-02-25 19:15:39 +0530

    modify brute and optimal approach for reverse string

--- ImpCodingChallenges/Challenges/reverseString.java
+++ ImpCodingChallenges/Challenges/reverseString.java
@@ -1,19 +1,47 @@
 package ImpCodingChallenges.Challenges;
 
-public class reverseString{
-    public static String reverseBruteForce(String s){
-        String reversed = "";
+// //Brute Force Approach
+// public class reverseString{
+//     public static String reverseBruteForce(String s){
+//         String reversed = "";
 
-        for(int i=s.length()-1;i>=0;i--){
-            reversed += s.charAt(i);
+//         for(int i=s.length()-1;i>=0;i--){
+//             reversed += s.charAt(i);
+//         }
+//         return reversed;
+//     }
+
+//     public static void main(String[] args){
+//         String input = "Hello";
+//         String reversed = reverseBruteForce(input);
+//         System.out.println("Original String : "+ input);
+//         System.out.println("Revesed string (Brute Force): "+ reversed);
+//     }
+// }
+
+//Optimal Approach
+public class reverseString{
+    public static String reverseOptimal(String s){
+        if(s==null || s.isEmpty()){
+            return s;
         }
-        return reversed;
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while(left < right){
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(chars);
     }
 
     public static void main(String[] args){
         String input = "Hello";
-        String reversed = reverseBruteForce(input);
+        String reversed = reverseOptimal(input);
         System.out.println("Original String : "+ input);
-        System.out.println("Revesed string (Brute Force): "+ reversed);
+        System.out.println("Reversed String(Optimal) : "+ reversed);
     }
 }
